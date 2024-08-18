6ebf0375d3d12c440174ee68b9b4c3c05845c910
Prabhat2056 <prabhat2056@gmail.com>
2024-08-18 12:13:39 +0545

    login

--- src/app/(login)/login/page.tsx
+++ src/app/(login)/login/page.tsx
@@ -1,10 +1,15 @@
 "use client"
 import React, { useState } from 'react'
 import CustomInput from '../../component/CustomInput'
+import Cookies from "js-cookie";
 
 const login = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+
+    const handleSubmit = () =>{
+      Cookies.set("token","abc")
+    }
   return (
     <div className="flex flex-col justify-center items-center bg-zinc-100">
       <form className="flex flex-col gap-3 w-1/3 my-5 p-2 rounded-xl bg-white border">
