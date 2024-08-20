410d3ab4b83837d93127e96fc09f81cbef7d63ef
Prabhat2056 <prabhat2056@gmail.com>
2024-08-20 11:41:37 +0545

    xyz

--- src/app/(login)/login/page.tsx
+++ src/app/(login)/login/page.tsx
@@ -2,18 +2,36 @@
 import React, { useState } from 'react'
 import CustomInput from '../../component/CustomInput'
 import Cookies from "js-cookie";
+import axios from 'axios';
 
 const login = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
-    const handleSubmit = () =>{
-      Cookies.set("token","abc")
+    const handleSubmit = async () =>{
+      // Cookies.set("token","abc")
+      try {
+        const response = await axios.post('http:localhost:8000',
+          {
+              email:email,
+              password: password
+        },
+        {
+          headers:{
+            "Content-Type": "Application/json"
+          }
+        })
+        const token= response.data.token;
+        Cookies.set("token",token);
+        
+      } catch (error) {
+        
+      }
     }
   return (
     <div className="flex flex-col justify-center items-center bg-zinc-100">
       <form className="flex flex-col gap-3 w-1/3 my-5 p-2 rounded-xl bg-white border">
-        <CustomInput
+        <CustomInput 
           label="Email"
           placeholder="Enter Email"
           value={email}
@@ -40,12 +58,14 @@ const login = () => {
 
         <button
           onClick={(e) => {
-            e.preventDefault();  
+            e.preventDefault(); 
+             
           }}
           className="border rounded-sm w-20 ml-32 bg-blue-400"
         >
           Login
         </button>
+        
   </form>
   </div>
   )
